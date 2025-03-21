22365571b3ba8d7352e958720b89185ba56229bb
ManuelFBS <manuelf.borrego@gmail.com>
2025-03-21 13:07:17 -0400

    Modificación en 'App.tsx'...

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,14 +2,32 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Navigate,
 } from 'react-router-dom';
 import { LoginPage, DashboardPage } from './pages';
 import { ProtectedRoute } from './components/security/ProtectedRoute';
+import { useAuthStore } from './store/authStore';
 
 function App() {
+    const { token } = useAuthStore();
+
     return (
         <Router>
             <Routes>
+                <Route
+                    path="/"
+                    element={
+                        token ? (
+                            <Navigate
+                                to="/dashboard"
+                                replace
+                            />
+                        ) : (
+                            <Navigate to="/login" replace />
+                        )
+                    }
+                />
+
                 <Route
                     path="/login"
                     element={<LoginPage />}
