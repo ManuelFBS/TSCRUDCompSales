08e76186e84cde4fc274cb2de2e32dfa30be3c90
ManuelFBS <manuelf.borrego@gmail.com>
2025-03-24 09:57:05 -0400

    Actualizaciones varias...

--- Frontend/src/pages/Dashboard/DashBoardPage.tsx
+++ Frontend/src/pages/Dashboard/DashBoardPage.tsx
@@ -1,32 +1,40 @@
-import { useAuthStore } from "../../store/authStore";
-import { logout } from "../../api/authService";
-import "../../styles/auth.css";
+import { useAuthStore } from '../../store/authStore';
+import { logout } from '../../api/authService';
+import '../../styles/auth.css';
 
 export const DashboardPage = () => {
-  const { user, fullName, logoutStore } = useAuthStore();
+    const { user, fullName, logoutStore } = useAuthStore();
 
-  const handleLogout = async () => {
-    try {
-      await logout();
-      logoutStore();
-    } catch (error) {
-      console.error("Logout failed...!", error);
-    }
-  };
+    const handleLogout = async () => {
+        try {
+            await logout();
+            logoutStore();
+        } catch (error) {
+            console.error('Logout failed...!', error);
+        }
+    };
 
-  return (
-    <div className="dashboard-container">
-      <div className="dashboard-card">
-        <h1 className="login-title mb-4">Dashboard</h1>
-        <div className="welcome-text">
-          👋 Bienvenido, <strong>{fullName} !</strong>
-          <span className="text-muted">Rol: {user?.role}</span>
+    return (
+        <div className="dashboard-container">
+            <div className="dashboard-card">
+                <h1 className="login-title mb-4">
+                    Dashboard
+                </h1>
+                <div className="welcome-text">
+                    👋 Bienvenido,{' '}
+                    <strong>{fullName} !</strong>
+                    <span className="text-muted">
+                        Rol: {user?.role}
+                    </span>
+                </div>
+                <button
+                    onClick={handleLogout}
+                    className="btn btn-logout"
+                >
+                    <i className="bi bi-box-arrow-right me-2"></i>
+                    Cerrar Sesión
+                </button>
+            </div>
         </div>
-        <button onClick={handleLogout} className="btn btn-logout">
-          <i className="bi bi-box-arrow-right me-2"></i>
-          Cerrar Sesión
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
--- Frontend/src/pages/Login-Logout/LoginPage.tsx
+++ Frontend/src/pages/Login-Logout/LoginPage.tsx
@@ -1,72 +1,85 @@
-import { useState } from "react";
-import { useAuthStore } from "../../store/authStore";
-import { login } from "../../api/authService";
-import { useNavigate } from "react-router-dom";
-import "../../styles/auth.css";
+import { useState } from 'react';
+import { useAuthStore } from '../../store/authStore';
+import { login } from '../../api/authService';
+import { useNavigate } from 'react-router-dom';
+import '../../styles/auth.css';
 
 export const LoginPage = () => {
-  const [user, setUser] = useState("");
-  const [password, setPassword] = useState("");
-  const { login: setAuth } = useAuthStore();
-  const navigate = useNavigate();
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const { login: setAuth } = useAuthStore();
+    const navigate = useNavigate();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
 
-    try {
-      const {
-        token,
-        user: userData,
-        fullName,
-      } = await login({
-        user,
-        password,
-      });
+        try {
+            const {
+                token,
+                user: userData,
+                fullName,
+            } = await login({
+                user,
+                password,
+            });
 
-      setAuth(token, userData, fullName);
-      navigate("/dashboard");
-    } catch (error) {
-      alert(`Login failed... Error: ${error}`);
-    }
-  };
+            setAuth(token, userData, fullName);
+            navigate('/dashboard');
+        } catch (error) {
+            alert(`Login failed... Error: ${error}`);
+        }
+    };
 
-  return (
-    <div className="container mt-5">
-      <div className="login-card">
-        <h1 className="login-title">Login</h1>
+    return (
+        <div className="container mt-5">
+            <div className="login-card">
+                <h1 className="login-title">Login</h1>
 
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="user" className="form-label fw-bold">
-              Usuario
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="user"
-              value={user}
-              onChange={(e) => setUser(e.target.value)}
-            />
-          </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label
+                            htmlFor="user"
+                            className="form-label fw-bold"
+                        >
+                            Usuario
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="user"
+                            value={user}
+                            onChange={(e) =>
+                                setUser(e.target.value)
+                            }
+                        />
+                    </div>
 
-          <div className="mb-4">
-            <label htmlFor="password" className="form-label fw-bold">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+                    <div className="mb-4">
+                        <label
+                            htmlFor="password"
+                            className="form-label fw-bold"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            value={password}
+                            onChange={(e) =>
+                                setPassword(e.target.value)
+                            }
+                        />
+                    </div>
 
-          <button type="submit" className="btn btn-login">
-            Login
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+                    <button
+                        type="submit"
+                        className="btn btn-login"
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
