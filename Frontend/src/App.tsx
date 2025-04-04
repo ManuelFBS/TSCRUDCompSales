5f1a2df304783fc7b83851a20451d75bd1b42471
ManuelFBS <manuelf.borrego@gmail.com>
2025-04-04 01:35:21 -0400

    Modificaciones en 'App.tsx', 'index.css' y 'main.tsx'...

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// import { useAuthStore } from './store/authStore';
+import LandingPage from './page/LandingPage';
+import LoginPage from './page/LoginPage';
+import DashboardPage from './page/DasboardPage';
+import ProtectedRoute from './components/common/ProtectedRoute';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage />} />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+                <Route
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <DashboardPage />
+                        </ProtectedRoute>
+                    }
+                >
+                    {/* Rutas anidadas para el dashboard */}
+                    {/* <Route path="employees/*" element={<EmployeeRoutes />} />
+                    <Route path="users/*" element={<UserRoutes />} /> */}
+                    {/* Otras rutas se agregarán después */}
+                </Route>
 
-export default App
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
